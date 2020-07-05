d2a5b97ab1a93a4e071bf326dab706dd1d3f936b
themorning <themorning84@gmail.com>
2020-07-05 19:18:59 +0900

    오류 메시지 가져오는 방법 / 처리할 예외 클래스의 추가(사용자 정의 예외 처리)

--- src/main/java/ee/swan/exception/ApiExceptionHandler.java
+++ src/main/java/ee/swan/exception/ApiExceptionHandler.java
@@ -1,15 +1,28 @@
 package ee.swan.exception;
 
 import ee.swan.api.ApiError;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+
+    private Map<Class<? extends Exception>, String> messageMappings =
+            Collections.unmodifiableMap(new LinkedHashMap<>() {
+                {
+                    put(HttpMessageNotReadableException.class, "Request body is invalid");
+                }
+            });
 
     @Override
     protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
@@ -19,8 +32,20 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     private ApiError createApiError(Exception ex) {
         ApiError apiError = new ApiError();
-        apiError.setMessage(ex.getMessage());
+        apiError.setMessage(resolveMessage(ex, ex.getMessage()));
         apiError.setDocumentationUrl("http://localhost:8080/api/errors");
         return apiError;
     }
+
+    private String resolveMessage(Exception ex, String defaultMessage) {
+        return messageMappings.entrySet().stream()
+                .filter(entry -> entry.getKey().isAssignableFrom(ex.getClass()))
+                .findFirst().map(Map.Entry::getValue).orElse(defaultMessage);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<Object> handleBookNotFoundException(BookResourceNotFoundException ex, WebRequest request) {
+        return handleExceptionInternal(ex, null, null, HttpStatus.NOT_FOUND, request);
+    }
+
 }
