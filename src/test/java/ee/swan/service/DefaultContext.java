0dd57442a42bf825337a20d8bd468c2f804f98df
themorning <themorning84@gmail.com>
2020-07-09 13:19:21 +0900

    프로파일 지정 @Profile("dev")

---
+++ src/test/java/ee/swan/service/DefaultContext.java
@@ -0,0 +1,19 @@
+package ee.swan.service;
+
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jndi.JndiTemplate;
+
+@Configuration
+@Profile("default")
+public class DefaultContext {
+
+    @Bean
+    DataSource dataSource() throws NamingException {
+        JndiTemplate jndiTemplate = new JndiTemplate();
+        return jndiTemplate.lookup("jdbc/dataSource", DataSource.class);
+    }
+}
---
+++ src/test/java/ee/swan/service/DevContext.java
@@ -0,0 +1,18 @@
+package ee.swan.service;
+
+import javax.sql.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+@Configuration
+@Profile("dev")
+public class DevContext {
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).build();
+    }
+}
