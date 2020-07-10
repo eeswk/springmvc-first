1c37fc74c74e5ae2a8f34b68f65757c1d23e41c7
themorning <themorning84@gmail.com>
2020-07-10 10:56:51 +0900

    테스트 데이터 소스 설정 (사용, 테스트용 Config)

--- src/main/java/ee/swan/config/AppConfig.java
+++ src/main/java/ee/swan/config/AppConfig.java
@@ -1,6 +1,8 @@
 package ee.swan.config;
 
 import java.util.Locale;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,11 +10,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.jndi.JndiTemplate;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 
 @Configuration
-@ComponentScan("ee.swan.service")
+@ComponentScan({"ee.swan.service", "ee.swan.repository"})
 public class AppConfig {
 
     @Bean
@@ -33,6 +36,12 @@ public class AppConfig {
         ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
         messageSource.setBasename("messages");
         return messageSource;
+    }
+
+    @Bean
+    public DataSource dataSource() throws NamingException {
+        JndiTemplate jndiTemplate = new JndiTemplate();
+        return jndiTemplate.lookup("jdbc/dataSource", DataSource.class);
     }
 
 }
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,4 @@
+CREATE TABLE account (
+    id CHAR(3) primary Key,
+    name VARCHAR (128)
+);
