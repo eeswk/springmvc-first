9e9b38a1c126c64f00eaab3a511622a31c9ab126
themorning <themorning84@gmail.com>
2020-07-10 13:00:14 +0900

    MockMvc 테스트

--- src/test/java/ee/swan/controller/WelcomeControllerTest.java
+++ src/test/java/ee/swan/controller/WelcomeControllerTest.java
@@ -4,6 +4,7 @@ import ee.swan.config.AppConfig;
 import ee.swan.config.WebMvcConfig;
 import ee.swan.service.MessageService;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -14,45 +15,49 @@ import org.springframework.test.context.ContextHierarchy;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
+
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 import static org.junit.Assert.*;
-/*
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextHierarchy({
-        @ContextConfiguration(classes = AppConfig.class),
-        @ContextConfiguration(classes = WebMvcConfig.class)
-})
-@WebAppConfiguration
- */
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
 public class WelcomeControllerTest {
     MockMvc mockMvc;
 
     @InjectMocks
-    MessageController controller;
+    WelcomeController controller;
 
     @Mock
     MessageService messageService;
 
+    @Autowired
+    WebApplicationContext context;
+
     @Before
     public void setupMockMvc() {
+       // this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                //서블릿 필터 추가
+        //    .addFilter(new CharacterEncodingFilter("UTF-8")).build();
+
         MockitoAnnotations.initMocks(this);
         this.mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
     }
 
-/*
-    @Autowired
-    WebApplicationContext context;
-
-    MockMvc mockMvc;
-
-    @Before
-    public void setupMockMvc() {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    @Test
+    public void testHome() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(forwardedUrl("index"));
     }
 
- */
+
+
+
+
 
 }
