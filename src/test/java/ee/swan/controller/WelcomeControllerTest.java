28a67bac93fe98ddbbea75aa5eb6fb7be2a35a8c
themorning <themorning84@gmail.com>
2020-07-10 13:53:31 +0900

    테스트 실행 결과 검증

--- src/test/java/ee/swan/controller/WelcomeControllerTest.java
+++ src/test/java/ee/swan/controller/WelcomeControllerTest.java
@@ -5,6 +5,7 @@ import ee.swan.config.TestConfig;
 import ee.swan.config.TransactionManagerConfig;
 import ee.swan.config.WebMvcConfig;
 import ee.swan.service.MessageService;
+import java.util.UUID;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,6 +13,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.ContextHierarchy;
 import org.springframework.test.context.jdbc.Sql;
@@ -23,6 +25,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
+import static org.hamcrest.core.StringContains.containsString;
 import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
@@ -55,6 +58,25 @@ public class WelcomeControllerTest {
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
                 .andExpect(forwardedUrl("/WEB-INF/index.jsp"));
+    }
+
+    @Test
+    public void testBooks() throws Exception {
+        mockMvc.perform(get("/books")
+                .param("name", "Spring")
+                .accept(MediaType.APPLICATION_JSON)
+                .header("X-Track-Id", UUID.randomUUID().toString()))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testBooksContent() throws Exception {
+        mockMvc.perform(get("/books"))
+                .andExpect(status().isOk())
+                .andExpect(content()
+                        .string(
+                                //containsString("[{\"bookId\":\"9791158390747\",\"name\":\"슬랙으로 협업하기\"}]"))
+                                containsString("9791158390747")));
     }
 
 
