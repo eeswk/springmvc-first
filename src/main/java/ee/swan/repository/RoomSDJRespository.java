46339040c99e9daa47eae02de8390ac991800efb
themorning <themorning84@gmail.com>
2020-07-25 16:32:04 +0900

    JPQL 사용 @query 와 메서드명 쿼리 생성

--- src/main/java/ee/swan/repository/RoomSDJRespository.java
+++ src/main/java/ee/swan/repository/RoomSDJRespository.java
@@ -1,8 +1,18 @@
 package ee.swan.repository;
 
 import ee.swan.domain.Room;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface RoomSDJRespository extends JpaRepository<Room, Integer> {
 
+    @Query("SELECT r FROM Room r WHERE r.roomName = :roomName")
+    List<Room> findByRoomName(@Param("roomName") String roomName);
+
+    @Query("UPDATE Room r SET r.capacity = :capacity")
+    Integer updateCapacityAll(@Param("capacity") Integer capacity);
+
+    List<Room> findByRoomNameAndCapacity(String roomName, Integer capacity);
 }
