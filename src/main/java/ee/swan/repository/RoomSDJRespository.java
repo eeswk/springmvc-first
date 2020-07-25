1c21ce78c71dc0df6bdb0b31a5617029a29d73d1
themorning <themorning84@gmail.com>
2020-07-25 15:30:50 +0900

    스프링 데이터 JPA JpaRepository 사용

---
+++ src/main/java/ee/swan/repository/RoomSDJRespository.java
@@ -0,0 +1,8 @@
+package ee.swan.repository;
+
+import ee.swan.domain.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomSDJRespository extends JpaRepository<Room, Integer> {
+
+}
